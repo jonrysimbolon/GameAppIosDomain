fb55b7cde32d172d684db110fa59ef45ccd641ef
Jonry Simbolon <jonrysimbolon19@gmail.com>
2026-08-06 20:26:39 +0700

    remove SwiftLintBuildToolPlugin from package

--- Package.swift
+++ Package.swift
@@ -16,9 +16,6 @@ let package = Package(
     targets: [
         .target(
             name: "Domain",
-            plugins: [
-                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
-            ]
         ),
         .testTarget(
             name: "DomainTests",
